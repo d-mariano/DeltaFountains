eb4c0ff8f21937031d4a44311f3dda97cd3612e7
Dave Mariano <dave.mariano1991@gmail.com>
2015-03-30 12:02:27 -0400

    Resolved error:  p not existing before client connect

--- Algorithms/pyDeltaServer/pyDeltaServer.py
+++ Algorithms/pyDeltaServer/pyDeltaServer.py
@@ -8,8 +8,8 @@
 #   A server script written in python that sends GCode commands
 #   to a delta robot.  The commands sent to the robot are determined
 #   by selections made on an Android or Web-Based client.  The delta bot 
-#   will have G-code compatible firmware.  This script uses printrun 
-#   libraries provided by kliment.
+#   will have G-code compatible firmware installed on its control board.  
+#   This script uses Printrun libraries provided by kliment on GitHub.
 #   
 #########################################################################
 import socket
@@ -18,8 +18,6 @@ import time
 import thread
 import signal
 import os
-
-from time import sleep
 
 from printrun.printcore import printcore
 from printrun.utils import setup_logging
@@ -125,27 +123,27 @@ def receiver( socket ):
 def sigintHandler(signum, frame):
     p.disconnect()
     s.close()
+    c.close()
     sys.exit(1)
 
 
-#####################################################
-# Begin script with logging to stderr and opening a 
-# connection to the control board
-#####################################################
+######################################################
+# Begin script with logging to stderr and binding to a 
+# socket on port 43000.  If a client connects, try and
+# connect to a printer.
+#######################################################
 # Setup logging to stderr
 setup_logging(sys.stderr)
 
 s = socket.socket()         # Create a socket object
-#host = socket.gethostname() 
-host = "192.168.50.1" # Get local machine name
+host = socket.gethostname() 
 port = 43000                # Reserve a port for your service
 try:
     s.bind((host,port))         # Bind to the port
 except Exception,e:
     print "Failed to bind socket to ", port
     print str(e)
-    s.close
-    p.disconnect
+    s.close()
     sys.exit(1)
 
 signal.signal(signal.SIGINT, sigintHandler)
@@ -154,20 +152,20 @@ s.listen(5)                 # Now wait for client connection
 print "%s listening on port %s" % (host, port)
 
 while True :
-    c, addr = s.accept()    # Establish connection with client
-    print "Got connection from ", addr
+    c = s.accept()    # Establish connection with client
+    print "Got connection from ", c
     
-#######   Connect to the printer  #########
+    #######   Connect to the printer  #########
     try:
-        p = printcore('/dev/ttyACM1', 115200)
+        p = printcore('/dev/ttyACM0', 115200)
     except Exception,e:
         print str(e)
-        try:
-            p = printcore('/dev/ttyACM1', 115200)
-        except Exception,e:
-            print str(e)
+        p = printcore('/dev/ttyACM1', 115200)
+        print "No printer, quitting."
+        s.close()
+        c.close()
 
-    sleep(2)   # Wait for printer to connect
+    timesleep(2)   # Wait for printer to connect
 
     thread.start_new_thread( receiver, ( c, ) ) # Begin receiver thread
     c.send("Server awaiting commands...\n")
